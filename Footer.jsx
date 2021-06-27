88dbe8bf3b5e76b23000d6e13b0940aca1c72283
Radhushani Samundeeshwaran <62767392+radhushani@users.noreply.github.com>
2021-06-27 14:18:21 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright by ShapeAi @ {new Date().getFullYear}</p>
+    </footer>
+  );
+}
+export default Footer;
